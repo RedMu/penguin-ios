258ef1d79ed769f86059c4d553203e90259b8526
danhall <dan.hall@red-mu.com>
2013-02-25 23:09:15 +0000

    Initial: Added Story controller with hardcoded stories and connect to Queue controller

---
+++ iPenguin/iPenguin/StoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  StoryViewController.h
+//  iPenguin
+//
+//  Created by Dan Hall on 25/02/2013.
+//  Copyright (c) 2013 Dan Hall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StoryViewController : UITableViewController
+
+@end
